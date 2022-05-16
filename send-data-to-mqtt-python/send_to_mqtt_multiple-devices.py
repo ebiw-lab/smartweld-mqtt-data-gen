96df6dd8f0d1cb32599de3d7d6ffe92b320db193
ebiw-lab <conference@ebiw.com>
2022-05-16 15:08:14 +0530

    backup file added & structure changed

--- send-data-to-mqtt-python/send_to_mqtt_multiple-devices.py
+++ send-data-to-mqtt-python/send_to_mqtt_multiple-devices.py
@@ -5,8 +5,7 @@ import random
 import time
 import pandas as pd
 
-
-from datetime import datetime as dt, timezone , timedelta
+from datetime import datetime as dt, timezone, timedelta
 
 import configparser
 
@@ -17,12 +16,10 @@ def count():
 
 
 def get_random_words(words_list):
-    return words_list[random.randint(0, len(words_list)-1)]
-
+    return words_list[random.randint(0, len(words_list) - 1)]
 
 
 def generate_data_array(hardware_id, dateinstance):
-
     # list to store JSON records
     # payload = {
     #               "type": "",
@@ -37,115 +34,112 @@ def generate_data_array(hardware_id, dateinstance):
     #                 "oid": "",
     #                 "dis": "",
     #                 "network": "",
-    #                 "tm": ""
+    #                 "tm": "",
+
     #               }
     #             }
 
     payload = {
-                  "d": [
-                        {
-                          "tag": "mstatus",
-                          "value": ""                        
-                        },
-                        {
-                          "tag": "tm",
-                          "value":  ""                       
-                        },
-                        {
-                          "tag": "current",
-                          "value":   ""                       
-                        },
-                        {
-                          "tag": "voltage",
-                          "value": ""                         
-                        },
-                        {
-                          "tag": "gasfr",
-                          "value":  ""                        
-                        },
-                        {
-                          "tag": "ambtemp",
-                          "value": ""                        
-                        },
+        "d": [
+            {
+                "tag": "mstatus",
+                "value": ""
+            },
+            {
+                "tag": "tm",
+                "value": ""
+            },
+            {
+                "tag": "current",
+                "value": ""
+            },
+            {
+                "tag": "voltage",
+                "value": ""
+            },
+            {
+                "tag": "gasfr",
+                "value": ""
+            },
+            {
+                "tag": "ambtemp",
+                "value": ""
+            },
 
-                        {
-                          "tag": "humidity",
-                          "value": ""                       
-                        },
-                        {
-                          "tag": "jointno",
-                          "value": ""                         
-                        },
-                        {
-                          "tag": "chuckspeed",
-                          "value": ""                       
-                        },
-                        {
-                          "tag":   "plc",
-                          "value":   ""                     
-                        },
-                        {
-                          "tag": "SYS_DATACARD_CAPACITY",
-                          "value": ""                       
-                        },
-                        {
-                          "tag": "SYS_DATACARD_FREE_SPACE",
-                          "value":  ""                      
-                        },
-                        {
-                          "tag": "DATALOG_ENABLE",
-                          "value": ""                       
-                        },
-                        {
-                          "tag": "DATALOG_ERROR",
-                          "value":  ""                      
-                        },
-                        {
-                          "tag": "oid",
-                          "value":  ""                      
-                        },
-                        {
-                          "tag": "dis",
-                          "value": ""                       
-                        },
-                        {
-                          "tag": "network",
-                          "value": ""                       
-                        }
-                    ]
-                }
+            {
+                "tag": "humidity",
+                "value": ""
+            },
+            {
+                "tag": "jointno",
+                "value": ""
+            },
+            {
+                "tag": "chuckspeed",
+                "value": ""
+            },
+            {
+                "tag": "plc",
+                "value": ""
+            },
+            {
+                "tag": "SYS_DATACARD_CAPACITY",
+                "value": ""
+            },
+            {
+                "tag": "SYS_DATACARD_FREE_SPACE",
+                "value": ""
+            },
+            {
+                "tag": "DATALOG_ENABLE",
+                "value": ""
+            },
+            {
+                "tag": "DATALOG_ERROR",
+                "value": ""
+            },
+            {
+                "tag": "oid",
+                "value": ""
+            },
+            {
+                "tag": "dis",
+                "value": ""
+            },
+            {
+                "tag": "network",
+                "value": ""
+            },
+            {
+                "tag": "wire_deposit",
+                "value": ""
+            }
+        ]
+    }
 
-    
-    #Initialization
-    mstatus                     =     ['1','0']
-    current                     =     ''
-    voltage                     =     ''
-    gasfr                       =     ''
-    ambtemp                     =     ''
-    humidity                    =     ''
-    jointno                     =     ''
-    chuckspeed                  =     ''
-    plc                         =     ['1','0']
-    SYS_DATACARD_CAPACITY       =     '7948206080.00'
-    SYS_DATACARD_FREE_SPACE     =     '7673053184.00'
-    DATALOG_ENABLE              =     ['1','0']
-    DATALOG_ERROR               =     '6'
-    oid                         =     ''
-    dis                         =     ''
-    network                     =     ''
+    # Initialization
+    mstatus = ['1', '0']
+    current = ''
+    voltage = ''
+    gasfr = ''
+    ambtemp = ''
+    humidity = ''
+    jointno = ''
+    chuckspeed = ''
+    plc = ['1', '0']
+    SYS_DATACARD_CAPACITY = '7948206080.00'
+    SYS_DATACARD_FREE_SPACE = '7673053184.00'
+    DATALOG_ENABLE = ['1', '0']
+    DATALOG_ERROR = '6'
+    oid = ''
+    dis = ''
+    network = ''
+    wire_deposit = ''
 
+    # Assigning generated values to payload
+    # payload['d'][0]['value'] = dateinstance.strftime('%Y-%m-%d %H:%M:%S')
 
-
-
-
-    #Assigning generated values to payload
-    #payload['d'][0]['value'] = dateinstance.strftime('%Y-%m-%d %H:%M:%S')
-
-
-    payload['d'][0]['value'] = get_random_words(mstatus) 
-
-    if payload['d'][0]['value'] == 'stop':
-        payload['d'][0]['value'] = get_random_words(mstatus)
+    payload['d'][0]['value'] = get_random_words(mstatus)
 
     if payload['d'][0]['value'] == 'stop':
         payload['d'][0]['value'] = get_random_words(mstatus)
@@ -153,152 +147,157 @@ def generate_data_array(hardware_id, dateinstance):
     if payload['d'][0]['value'] == 'stop':
         payload['d'][0]['value'] = get_random_words(mstatus)
 
-    #Generating tm
+    if payload['d'][0]['value'] == 'stop':
+        payload['d'][0]['value'] = get_random_words(mstatus)
+
+    # Generating tm
     payload['d'][1]['value'] = dateinstance.strftime('%Y-%m-%d %H:%M:%S')
 
-    #Generating current
+    # Generating current
     if payload['d'][0]['value'] == '1':
-        current = random.randint(80, 90)   
+        current = random.randint(80, 90)
     payload['d'][2]['value'] = str(current)
 
-    #Generating voltage
+    # Generating voltage
     if payload['d'][0]['value'] == '1':
-        voltage = random.randint(18, 24)   
+        voltage = random.randint(18, 24)
     payload['d'][3]['value'] = str(voltage)
 
-    #Generating gasfr
+    # Generating gasfr
     if payload['d'][0]['value'] == '1':
-        gasfr = random.randint(18, 22)   
+        gasfr = random.randint(18, 22)
     payload['d'][4]['value'] = str(gasfr)
 
-    #Generating ambtemp
+    # Generating ambtemp
     if payload['d'][0]['value'] == '1':
-        ambtemp = random.randint(27, 30)   
-    payload['d'][5]['value'] = str(ambtemp)  
+        ambtemp = random.randint(27, 30)
+    payload['d'][5]['value'] = str(ambtemp)
 
-    #Generating humidity
+    # Generating humidity
     if payload['d'][0]['value'] == '1':
-        humidity = random.randint(40, 90)   
+        humidity = random.randint(40, 90)
     payload['d'][6]['value'] = str(humidity)
 
-    #Generating jointno
+    # Generating jointno
     if payload['d'][0]['value'] == '1':
-        jointno = random.randint(1, 100)   
+        jointno = random.randint(1, 100)
     payload['d'][7]['value'] = str(jointno)
 
-    #Generating chuckspeed
+    # Generating chuckspeed
     if payload['d'][0]['value'] == '1':
-        chuckspeed = random.randint(4, 10)   
+        chuckspeed = random.randint(4, 10)
     payload['d'][8]['value'] = str(chuckspeed)
 
-    #Generating plc
+    # Generating plc
     if payload['d'][0]['value'] == '1':
         payload['d'][9]['value'] = get_random_words(plc)
 
-    #Generating SYS_DATACARD_CAPACITY
+    # Generating SYS_DATACARD_CAPACITY
     if payload['d'][0]['value'] == '1':
         payload['d'][10]['value'] = str(SYS_DATACARD_CAPACITY)
 
-    #Generating SYS_DATACARD_FREE_SPACE
+    # Generating SYS_DATACARD_FREE_SPACE
     if payload['d'][0]['value'] == '1':
         payload['d'][11]['value'] = str(SYS_DATACARD_FREE_SPACE)
 
-    #Generating DATALOG_ENABLE
+    # Generating DATALOG_ENABLE
     if payload['d'][0]['value'] == '1':
         payload['d'][12]['value'] = str(get_random_words(DATALOG_ENABLE))
 
-    #Generating DATALOG_ERROR
+    # Generating DATALOG_ERROR
     if payload['d'][0]['value'] == '1':
         DATALOG_ERROR = random.randint(1, 9)
     payload['d'][13]['value'] = str(DATALOG_ERROR)
 
-    #Generating oid
+    # Generating oid
     if payload['d'][0]['value'] == '1':
         payload['d'][14]['value'] = str(oid)
 
-    #Generating dis
+    # Generating dis
     if payload['d'][0]['value'] == '1':
         payload['d'][15]['value'] = str(dis)
 
-    #Generating network
+    # Generating network
     if payload['d'][0]['value'] == '1':
         payload['d'][16]['value'] = str(network)
 
+    # Generating wire deposit
+    if payload['d'][0]['value'] == '1':
+        payload['d'][17]['value'] = str(wire_deposit)
+
     return payload
 
-        
-def on_publish(client, userdata, mid):
-    print ("Message Published...")
 
-      
-def date_difference(dt1,dt2):
+def on_publish(client, userdata, mid):
+    print("Message Published...")
+
+
+def date_difference(dt1, dt2):
     if dt1 == dt2:
-        return 1*60*24
+        return 1 * 60 * 24
     else:
         dif = dt2 - dt1
-        return dif.days*60*60*24
+        return dif.days * 60 * 60 * 24
 
 
 if __name__ == "__main__":
-    
+
+
     # Program Execution Time
-   #exec_time = time.time()
+    # exec_time = time.time()
 
     config = configparser.RawConfigParser()
     config.read('setup/config.cfg')
     mqtt_conf = config['mqtt_details']
     file = config['file']
+
+
     machine_list_filename = file['source_file_name']
 
     # Get List Of mid to subcribe
     machinesDF = pd.read_excel(machine_list_filename).astype(str)
     mid_list = machinesDF.to_numpy().flatten()
 
-    #print(type(mid_list))
-    #print(mid_list)
+    # print(type(mid_list))
+    # print(mid_list)
 
-    #mid_list = ['869247043362190']
-    
+    # mid_list = ['869247043362190']
+
     startDate = dt.strptime(dt.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
     endDate = dt.strptime("2022-12-31" + " 23:59:00", '%Y-%m-%d %H:%M:%S')
 
     size = date_difference(startDate, endDate)
-    #size = 30
+    # size = 30
 
-  
     # MQTT connection
     client = mqtt.Client()
-
     client.connect(mqtt_conf['host'], int(mqtt_conf['port']), int(mqtt_conf['keepalive']))
 
-    
-    for i in range(0,size):
+    for i in range(0, size):
 
         # Program Execution Time
-        #exec_time = time.time()
-        
+        # exec_time = time.time()
+
         payload = {}
-        
+
         date_instance = startDate + timedelta(seconds=i)
 
         for hardware_id in mid_list:
             payload = json.dumps(generate_data_array(hardware_id, date_instance))
-            #print(payload)
-            #print(type(str(payload).encode('utf-8')))
+            # print(payload)
+            # print(type(str(payload).encode('utf-8')))
             data = str(payload).encode('utf-8')
-            #print(data)
-            
-            client.publish("data/ecu1051/"+hardware_id, data)
+            # print(data)
 
-        #print("---Execution time: %s seconds ---" % (time.time() - exec_time))
-            
+            client.publish("data/ecu1051/" + hardware_id, data)
+
+        print(payload)
+        # print("---Execution time: %s seconds ---" % (time.time() - exec_time))
+
         time.sleep(0.99865)
-        #time.sleep(3)
+        # time.sleep(3)
 
-    
     print("Execution Started...")
 
     print("\nTask Completed.")
-    #print("--- %s seconds ---" % (time.time() - exec_time))
-
-
+    # print("--- %s seconds ---" % (time.time() - exec_time))
